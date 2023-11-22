ce9b8b9b6149d5c5585f8c41b34befbaf6df7dd6
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-22 16:37:55 +0200

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Leo  on 22.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
